a70c2f9bdb1c9a2eb48aaee71ff3b8b3a52f22dc
iamspd <iamspd.ai9@gmail.com>
2021-05-28 09:27:27 -0400

    MainActivity - ListView implemented

--- app/src/main/java/com/example/listviewdemo/MainActivity.java
+++ app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.listviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     // widgets
     private ListView mListView;
+
+    // variables
+    private ArrayList<String> myFamily = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +23,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mListView = findViewById(R.id.myListView);
+
+        myFamily = (ArrayList<String>) Arrays.asList("Priyank", "Strange", "Emma");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, myFamily);
+
+        mListView.setAdapter(arrayAdapter);
+
     }
 }
