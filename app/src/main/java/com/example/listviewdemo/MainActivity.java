b4b768ce2748864a6b9b3312eb1dd57cc07949aa
iamspd <iamspd.ai9@gmail.com>
2021-05-28 09:49:55 -0400

    MainActivity - Method setOnItemClickListener added

--- app/src/main/java/com/example/listviewdemo/MainActivity.java
+++ app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.listviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -29,6 +33,15 @@ public class MainActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, myFamily);
 
         mListView.setAdapter(arrayAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(MainActivity.this, myFamily.get(position), Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 }
